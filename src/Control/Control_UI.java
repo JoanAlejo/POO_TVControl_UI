0597a755331e717ce4a1c46c66a3386c5139f860
sigmotoa <si.galvismotoa@gmail.com>
2026-04-20 16:19:30 -0500

    Fixing the call to be render

--- src/Control/Control_UI.java
+++ src/Control/Control_UI.java
@@ -1,4 +1,42 @@
 package Control;
 
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 public class Control_UI {
-}
+    private JPanel mainPanel;
+    private JButton button1;
+    private JButton button2;
+    private JButton button3;
+    private JButton button4;
+    private JLabel vol_text;
+
+
+    public int vol = 0;
+
+    public Control_UI() {
+        button1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+                vol = vol - 1;
+                vol_text.setText(String.valueOf(vol));
+            }
+        });
+        button2.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                vol = vol + 1;
+                vol_text.setText(String.valueOf(vol));
+            }
+        });
+    }
+        public JPanel getMainPanel()
+        {
+            return mainPanel;
+        }
+    }
+
+
+
---
+++ src/Main.java
@@ -0,0 +1,21 @@
+import Control.Control_UI;
+import Control.Control_UI.*;
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+        SwingUtilities.invokeLater(() -> {
+            Control_UI ui = new Control_UI();
+
+            JFrame frame = new JFrame("Control TV");
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.setContentPane(ui.getMainPanel());
+            //frame.setSize(800, 600);
+            frame.pack(); //size
+            frame.setLocationRelativeTo(null);
+            frame.setVisible(true);
+        });
+}
+}
+
